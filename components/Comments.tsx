4d8bf32ea4c3ee97c5a5b77a0ff3c9295761fd15
joey Ma <joejoey.ma@gmail.com>
2024-09-25 20:57:02 +0800

    blog init for vercel

---
+++ components/Comments.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { Comments as CommentsComponent } from 'pliny/comments'
+import { useState } from 'react'
+import siteMetadata from '@/data/siteMetadata'
+
+export default function Comments({ slug }: { slug: string }) {
+  const [loadComments, setLoadComments] = useState(false)
+
+  if (!siteMetadata.comments?.provider) {
+    return null
+  }
+  return (
+    <>
+      <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
+      {/* {loadComments ? (
+        <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
+      ) : (
+        <button onClick={() => setLoadComments(true)}>Load Comments</button>
+      )} */}
+    </>
+  )
+}
