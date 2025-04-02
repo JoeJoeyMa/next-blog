c53a4a95ba0a2bc74fe8993c2477da7e652a3258
joejoey.ma@gmail.com <joejoey.ma@gmail.com>
2025-04-02 18:23:16 +0800

    refactor: update max-width values in SectionContainer and WideContainer components

--- components/WideContainer.tsx
+++ components/WideContainer.tsx
@@ -5,5 +5,5 @@ interface WideContainerProps {
 }
 
 export default function WideContainer({ children }: WideContainerProps) {
-  return <div className="mx-auto max-w-6xl px-4 sm:px-6 xl:px-0">{children}</div>
+  return <div className="mx-auto max-w-8xl px-4 sm:px-6 xl:px-0">{children}</div>
 }
